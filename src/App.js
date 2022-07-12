4d978daaed7017955c75798405fac20a9795122e
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-12 14:11:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,51 @@
-import React, { Fragment } from "react";
-import { Box, CssBaseline } from "@mui/material";
+import React, { Fragment, useEffect, useState } from "react";
+import { Box, Container, CssBaseline } from "@mui/material";
+import { commerce } from "./lib/commerce";
 
-function App() {
+import TopBanner from "./components/TopBanner/TopBanner";
+import AppBar from "./components/AppBar/AppBar";
+
+const App = ({ props }) => {
+  const [products, setProducts] = useState([]);
+
+  console.log(products);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = () => {
+    commerce.products
+      .list()
+      .then(({ data }) => {
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.log("There was an error fetching the products", error);
+      });
+  };
+
   return (
     <Fragment>
       <CssBaseline />
-      <Box>
-        <p>test</p>
-      </Box>
+      <TopBanner />
+      <AppBar />
+      <Container maxWidth="xl">
+        <Box sx={{ my: -5 }}>
+          <Container>
+            {[...new Array(52)]
+              .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+              )
+              .join("\n")}
+          </Container>
+        </Box>
+      </Container>
     </Fragment>
   );
-}
+};
 
 export default App;
