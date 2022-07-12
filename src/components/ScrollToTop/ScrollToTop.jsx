3adf2be62966ca033c333532b46210585842bc66
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-12 14:11:20 +0100

    Create ScrollToTop.jsx

---
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Box, Fade, useScrollTrigger } from "@mui/material";
+import PropTypes from "prop-types";
+
+const ScrollToTop = ({ children, window }) => {
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      "#back-to-top-anchor"
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: "center",
+      });
+    }
+  };
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: "fixed", bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  );
+};
+
+export default ScrollToTop;
+
+ScrollToTop.prototype = {
+  children: PropTypes.element.isRequired,
+  window: PropTypes.func,
+};
