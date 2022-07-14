b6a06740b47524d1169808a31e50b6c067af0270
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-14 13:39:49 +0100

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -5,8 +5,12 @@ import {
   Fab,
   Toolbar,
   Typography,
+  Avatar,
+  IconButton,
+  Badge,
 } from "@mui/material";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
 
 import ElevateScroll from "./ElevateScroll";
 import ScrollToTop from "../ScrollToTop/ScrollToTop";
@@ -15,14 +19,29 @@ const AppBar = () => {
   return (
     <Fragment>
       <ElevateScroll>
-        <MUIAppBar elevation={0} position="sticky">
-          <Toolbar>
-            <Container maxWidth="xl">
+        <MUIAppBar elevation={0} position="sticky" color="inherit">
+          <Container maxWidth="xl">
+            <Toolbar
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                height: 70,
+              }}
+            >
               <Typography variant="h6" component="div">
-                App bar
+                Logo
               </Typography>
-            </Container>
-          </Toolbar>
+
+              <Badge badgeContent={4} color="primary">
+                <Avatar>
+                  <IconButton aria-label="shopping-cart">
+                    <LocalMallOutlinedIcon />
+                  </IconButton>
+                </Avatar>
+              </Badge>
+            </Toolbar>
+          </Container>
         </MUIAppBar>
       </ElevateScroll>
       <Toolbar id="back-to-top-anchor" />
