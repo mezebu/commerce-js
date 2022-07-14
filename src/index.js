53eeadddda0d1fcc593b9e18148f0f254f0616b2
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-14 13:40:17 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "normalize.css";
 import "./index.css";
+import AppThemeProvider from "./themes/AppThemeProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <AppThemeProvider>
     <App />
-  </React.StrictMode>
+  </AppThemeProvider>
 );
