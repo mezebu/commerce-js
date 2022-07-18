1911b43821ba7950defaadb26b11ca410b07d9ef
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-18 10:09:15 +0100

    Update Hero.jsx

--- src/components/HeroPage/Hero.jsx
+++ src/components/HeroPage/Hero.jsx
@@ -1,22 +1,28 @@
-import { Grid } from "@mui/material";
+import { Grid, Container } from "@mui/material";
 import React from "react";
 import SwipeableCard from "./SwipeableCard";
 import HeroCardOne from "./HeroCardOne";
 import HeroCardTwo from "./HeroCardTwo";
 import { Outlet } from "react-router-dom";
+import HeroWidgets from "./HeroWidgets";
 
 const Hero = () => {
   return (
-    <Grid container spacing={3}>
-      <Grid item lg={4} md={4} sm={12} xs={12}>
-        <SwipeableCard />
+    <Container>
+      <Grid container spacing={3}>
+        <Grid item lg={4} md={4} sm={12} xs={12}>
+          <SwipeableCard />
+        </Grid>
+        <Grid item lg={8} md={8} sm={12} xs={12}>
+          <HeroCardOne />
+          <HeroCardTwo />
+        </Grid>
       </Grid>
-      <Grid item lg={8} md={8} sm={12} xs={12}>
-        <HeroCardOne />
-        <HeroCardTwo />
-      </Grid>
+
+      <HeroWidgets />
+
       <Outlet />
-    </Grid>
+    </Container>
   );
 };
 
