760ba56b436b1d122a5430e956c0a4e84f121b63
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-19 10:54:38 +0100

    Create Fade.jsx

---
+++ src/components/FilterModal/Fade.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useSpring, animated } from "react-spring";
+import PropTypes from "prop-types";
+
+const Fade = React.forwardRef(function Fade(props, ref) {
+  const { in: open, children, onEnter, onExited, ...other } = props;
+  const style = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: open ? 1 : 0 },
+    onStart: () => {
+      if (open && onEnter) {
+        onEnter();
+      }
+    },
+    onRest: () => {
+      if (!open && onExited) {
+        onExited();
+      }
+    },
+  });
+
+  return (
+    <animated.div ref={ref} style={style} {...other}>
+      {children}
+    </animated.div>
+  );
+});
+
+export default Fade;
+
+Fade.propTypes = {
+  children: PropTypes.element,
+  in: PropTypes.bool.isRequired,
+  onEnter: PropTypes.func,
+  onExited: PropTypes.func,
+};
