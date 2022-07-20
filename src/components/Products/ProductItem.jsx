893ecdc9a36b0b1ee0728b6cadb2284937f7e48f
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-20 12:53:47 +0100

    Update ProductItem.jsx

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,13 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Card, CardMedia } from "@mui/material";
+import {
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
+import AddShoppingCartRoundedIcon from "@mui/icons-material/AddShoppingCartRounded";
+import RemoveRedEyeRoundedIcon from "@mui/icons-material/RemoveRedEyeRounded";
+import { useNavigate } from "react-router-dom";
+import { ProductWrapper } from "./styles";
 
 const ProductItem = ({ product }) => {
-  console.log(product);
+  const navigate = useNavigate();
+
+  const handleDescription = (id) => {
+    navigate(`/${id}`);
+  };
+
   return (
-    <Card elevation={0} variant="outlined">
+    <ProductWrapper elevation={0} variant="outlined">
       <CardMedia component="img" height="190" image={product.image?.url} />
-    </Card>
+      <CardContent>
+        <Typography sx={{ fontWeight: 600 }} gutterBottom variant="body1">
+          {product.name}
+        </Typography>
+        <Typography sx={{ fontWeight: 500 }} gutterBottom variant="body2">
+          {product.price.formatted_with_symbol}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <Button startIcon={<AddShoppingCartRoundedIcon />}>Add Item</Button>
+        <Button
+          startIcon={<RemoveRedEyeRoundedIcon />}
+          onClick={() => handleDescription(product.id)}
+        >
+          Description
+        </Button>
+      </CardActions>
+    </ProductWrapper>
   );
 };
 
