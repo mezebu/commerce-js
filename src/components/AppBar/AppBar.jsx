18dba93b7af6ba6b36aa0831df25b79cdb2bc767
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-22 01:49:27 +0100

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,31 +1,22 @@
-import React, { Fragment } from "react";
-import {
-  Typography,
-  Avatar,
-  IconButton,
-  Badge,
-  Box,
-  useMediaQuery,
-} from "@mui/material";
-import { AppBar as MUIAppBar, Container, Toolbar } from "@mui/material";
-import { styled, useTheme } from "@mui/material/styles";
+import React from "react";
+import { Typography, Avatar, IconButton, Badge } from "@mui/material";
+import { AppBar as MUIAppBar, Box, Toolbar } from "@mui/material";
 import PropTypes from "prop-types";
 import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
 import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
 
 import { Search, SearchIconWrapper, StyledInputBase } from "./styles";
+import { Link } from "react-router-dom";
 
-const AppBar = ({ query, handleChange, searchProduct }) => {
-  const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down("sm"));
+//prettier-ignore
+const AppBar = ({ query, handleChange, searchProduct, handleDrawerToggle }) => {
 
   return (
-    <Fragment>
+    <Box sx={{ flexGrow: 1 }}>
       <MUIAppBar
+        component="nav"
         elevation={0}
-        position="sticky"
-        color="transparent"
         sx={{
           background: "rgba( 255, 255, 255, 0.35 )",
           backdropFilter: "blur( 4px )",
@@ -33,46 +24,48 @@ const AppBar = ({ query, handleChange, searchProduct }) => {
           border: "1px solid rgba( 255, 255, 255, 0.18 )",
         }}
       >
-        <Container maxWidth="xl">
-          <Toolbar
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
-            <Typography
-              sx={{ display: matches ? "none" : "block" }}
-              variant="h6"
-              component="div"
-            >
-              Logo
-            </Typography>
-            <Box sx={{ display: "flex" }}>
-              <Search onSubmit={searchProduct}>
-                <SearchIconWrapper>
-                  <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                  placeholder="Search Product"
-                  inputProps={{ "aria-label": "search" }}
-                  value={query}
-                  onChange={handleChange}
-                />
-              </Search>
-              <Badge badgeContent={4} color="primary">
-                <Avatar>
-                  <IconButton aria-label="shopping-cart">
-                    <LocalMallOutlinedIcon />
-                  </IconButton>
-                </Avatar>
-              </Badge>
-            </Box>
-          </Toolbar>
-        </Container>
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            <Link to='/'>Logo</Link>
+            
+          </Typography>
+            <Search onSubmit={searchProduct}>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search Product"
+                inputProps={{ "aria-label": "search" }}
+                value={query}
+                onChange={handleChange}
+              />
+          </Search>
+          <Box sx={{p:2}}>
+            <Badge badgeContent={4} color="primary">
+              <Avatar>
+                <IconButton aria-label="shopping-cart">
+                  <LocalMallOutlinedIcon />
+                </IconButton>
+              </Avatar>
+            </Badge>
+          </Box>         
+        </Toolbar>
       </MUIAppBar>
-      <Offset />
-    </Fragment>
+    </Box>
   );
 };
 
@@ -82,4 +75,5 @@ AppBar.propTypes = {
   handleChange: PropTypes.func,
   searchProduct: PropTypes.func,
   query: PropTypes.string,
+  handleDrawerToggle: PropTypes.func,
 };
