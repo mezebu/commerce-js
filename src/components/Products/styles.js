09b89b295d0d85f00daab02193a1acf75a05d3da
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-22 01:48:44 +0100

    Update styles.js

--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -1,9 +1,8 @@
 import { Card } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-export const ProductWrapper = styled(Card)(({ theme }) => ({
+export const ProductWrapper = styled(Card)(() => ({
   background: "rgba( 255, 255, 255, 0.2 )",
-  boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
   backdropFilter: "blur (4px)",
   WebkitBackdropFilter: "blur(4px)",
 }));
