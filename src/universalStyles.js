c5b45a2784e3a900cfc0c600579f945f5f20100b
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-23 12:28:01 +0100

    Create universalStyles.js

---
+++ src/universalStyles.js
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const CenteredFlexItems = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+});
