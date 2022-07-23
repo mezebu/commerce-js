99948f11f80eb2d2b6b3a70d0f30a460edf2686c
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-23 03:52:15 +0100

    Update ProductsList.jsx

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { Box, Container, Grid } from "@mui/material";
 import ProductItem from "./ProductItem";
 import SortPanel from "../SortPanel/SortPanel";
+import { useCommerce } from "../../contexts/CommerceContext";
 
-const ProductsList = ({ products, sortProducts }) => {
+const ProductsList = () => {
+  const { products, handleAddToCart } = useCommerce();
+
   return (
     <Container>
       <Box sx={{ mb: 5 }}>
         <Box sx={{ mb: 3 }}>
-          <SortPanel sortProducts={sortProducts} />
+          <SortPanel />
         </Box>
         <Grid container spacing={1}>
           {products.map((product) => (
             <Grid key={product.id} item lg={3} md={4} sm={6} xs={12}>
-              <ProductItem product={product} />
+              <ProductItem product={product} onAddToCart={handleAddToCart} />
             </Grid>
           ))}
         </Grid>
@@ -24,8 +26,3 @@ const ProductsList = ({ products, sortProducts }) => {
 };
 
 export default ProductsList;
-
-ProductsList.propTypes = {
-  products: PropTypes.array,
-  sortProducts: PropTypes.func,
-};
