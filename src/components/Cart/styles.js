4c93c7b8c004ee81a4a04f1baadde92dcdd30f97
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-24 02:09:38 +0100

    Update styles.js

--- src/components/Cart/styles.js
+++ src/components/Cart/styles.js
@@ -1,4 +1,4 @@
-import { TableCell, tableCellClasses } from "@mui/material";
+import { Box, TableCell, tableCellClasses } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 export const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -9,4 +9,14 @@ export const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
   },
+}));
+
+export const ActionButtons = styled(Box)(({ theme }) => ({
+  border: "1px solid rgba(145, 158, 171, 0.32)",
+  borderRadius: theme.shape.borderRadius,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  width: 90,
+  height: 35,
 }));
