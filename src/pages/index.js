c7af8498a7acb6b72a871b1b70f4084f4545fea7
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-25 10:43:21 +0100

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Hero } from "./HeroPage/Hero";
+export { default as ProductsList } from "./Products/ProductsList";
+export { default as ProductDescription } from "./Products/ProductDescription";
+export { default as Cart } from "./Cart/Cart";
+export { default as CheckOut } from "./CheckOut/CheckOut";
+export { default as NoMatch } from "./NoMatch/NoMatch";
+export { default as SearchPage } from "./SearchPage/SearchPage";
