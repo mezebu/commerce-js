27c2da26abfabc08a9e4b6663d33e02053a361e7
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-25 10:40:57 +0100

    Delete universalStyles.js

--- src/universalStyles.js
+++
@@ -1,8 +0,0 @@
-import { Box } from "@mui/material";
-import { styled } from "@mui/material/styles";
-
-export const CenteredFlexItems = styled(Box)({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
-});
