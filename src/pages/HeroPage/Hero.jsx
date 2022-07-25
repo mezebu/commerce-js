dc5031db7e2e0f952e673bce89aa9466ffb3bdc0
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-25 10:42:19 +0100

    Create Hero.jsx

---
+++ src/pages/HeroPage/Hero.jsx
@@ -0,0 +1,26 @@
+import { Grid } from "@mui/material";
+import React from "react";
+import SwipeableCard from "./SwipeableCard";
+import HeroCardOne from "./HeroCardOne";
+import HeroCardTwo from "./HeroCardTwo";
+import HeroWidgets from "./HeroWidgets";
+
+const Hero = () => {
+  return (
+    <>
+      <Grid container spacing={3}>
+        <Grid item lg={4} md={4} sm={12} xs={12}>
+          <SwipeableCard />
+        </Grid>
+        <Grid item lg={8} md={8} sm={12} xs={12}>
+          <HeroCardOne />
+          <HeroCardTwo />
+        </Grid>
+      </Grid>
+      <HeroWidgets />
+
+    </>
+  );
+};
+
+export default Hero;
