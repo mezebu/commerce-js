034315353296b40e3509be2e8baeeda411bdfa6c
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-25 10:39:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,37 +2,35 @@ import React from "react";
 import { Box, Container, CssBaseline } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 
-import Hero from "./components/HeroPage/Hero";
+// prettier-ignore
+import { Hero, ProductsList, ProductDescription, Cart, NoMatch, CheckOut, SearchPage,  } from "./pages";
+
 import Footer from "./components/Footer/Footer";
-import ProductsList from "./components/Products/ProductsList";
-import NoMatch from "./components/NoMatch/NoMatch";
-import ProductDescription from "./components/Products/ProductDescription";
 import Header from "./components/Header/Header";
 
 import { CommerceContext } from "./contexts/CommerceContext";
-import Cart from "./components/Cart/Cart";
 
 const App = () => {
   return (
     <CommerceContext>
-      <Box>
-        <CssBaseline />
-        <Header />
-        <Container maxWidth="xl">
-          <Box component="main" sx={{ p: 3 }}>
-            <Routes>
-              <Route path="/">
-                <Route index element={<Hero />} />
-                <Route path="products" element={<ProductsList />} />
-                <Route path=":productId" element={<ProductDescription />} />
-                <Route path="cart" element={<Cart />} />
-                <Route path="*" element={<NoMatch />} />
-              </Route>
-            </Routes>
-          </Box>
-        </Container>
-        <Footer />
-      </Box>
+      <CssBaseline />
+      <Header />
+      <Container maxWidth="xl">
+        <Box component="main" sx={{ p: 3 }}>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Hero />} />
+              <Route path="products" element={<ProductsList />} />
+              <Route path=":productId" element={<ProductDescription />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="search" element={<SearchPage />} />
+              <Route path="checkout" element={<CheckOut />} />
+              <Route path="*" element={<NoMatch />} />
+            </Route>
+          </Routes>
+        </Box>
+      </Container>
+      <Footer />
     </CommerceContext>
   );
 };
