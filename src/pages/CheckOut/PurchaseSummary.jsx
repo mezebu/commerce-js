0e400f5b09496700626ab66acb0af550142aedbb
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-29 00:17:45 +0100

    Create PurchaseSummary.jsx

---
+++ src/pages/CheckOut/PurchaseSummary.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Box, Typography, List, ListItem, Avatar } from "@mui/material";
+import { Divider, ListItemText, ListItemAvatar } from "@mui/material";
+
+import { useCommerce } from "../../contexts/CommerceContext";
+
+const PurchaseSummary = () => {
+  const { checkoutToken } = useCommerce();
+
+  return (
+    <Box>
+      <Typography variant="h6" align="center" sx={{ mt: 2 }} gutterBottom>
+        Order Summary
+      </Typography>
+      {checkoutToken.live.line_items.map(
+        ({ id, media, name, quantity, line_total }) => (
+          <List key={id} sx={{ width: "100%", bgcolor: "background.paper" }}>
+            <ListItem
+              secondaryAction={
+                <Typography>{line_total?.formatted_with_symbol}</Typography>
+              }
+            >
+              <ListItemAvatar>
+                <Avatar alt={name} src={media?.source} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={
+                  <Typography variant="subtitle1" sx={{ fontWeight: 600 }}>
+                    {name}
+                  </Typography>
+                }
+                secondary={
+                  <React.Fragment>
+                    <Typography
+                      sx={{ display: "inline" }}
+                      component="span"
+                      variant="subtitle2"
+                      color="text.primary"
+                    >
+                      Quantity: {quantity}
+                    </Typography>
+                  </React.Fragment>
+                }
+              />
+            </ListItem>
+          </List>
+        )
+      )}
+      <Box sx={{ p: 3 }}>
+        <Typography variant="subtitle1" sx={{ fontWeight: 600 }}>
+          Total
+        </Typography>
+        <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
+          {checkoutToken.live.subtotal.formatted_with_symbol}
+        </Typography>
+      </Box>
+      <Divider />
+    </Box>
+  );
+};
+
+export default PurchaseSummary;
