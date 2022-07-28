987268c40cafa662c6cdfbec172f5412f75ce7a9
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-29 00:17:34 +0100

    Create CustomeTextField.jsx

---
+++ src/pages/CheckOut/CustomeTextField.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { TextField, Grid } from "@mui/material";
+import { useFormContext, Controller } from "react-hook-form";
+import PropTypes from "prop-types";
+
+const CustomeTextField = ({ name, label, value }) => {
+  const { control } = useFormContext();
+  return (
+    <Grid item xs={12} sm={6}>
+      <Controller
+        render={({ field }) => (
+          <TextField
+            variant="filled"
+            label={label}
+            {...field}
+            fullWidth
+            required
+          />
+        )}
+        name={name}
+        control={control}
+        defaultValue={""}
+      />
+    </Grid>
+  );
+};
+
+export default CustomeTextField;
+
+CustomeTextField.propTypes = {
+  name: PropTypes.string,
+  label: PropTypes.string,
+  required: PropTypes.bool,
+};
