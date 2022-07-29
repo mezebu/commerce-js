8a006657e8faa57e88b39fc4149f492d124e0e18
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-29 12:20:34 +0100

    Update SearchPage.jsx

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Box, Container, Grid } from "@mui/material";
 
 import QueryItem from "./QueryItem";
+import { useCommerce } from "../../contexts/CommerceContext";
 
-const SearchPage = ({ queryData }) => {
+const SearchPage = () => {
+  const { queryData, handleAddToCart } = useCommerce();
   return (
     <Container>
       <Box sx={{ mb: 5 }}>
         <Grid container spacing={1}>
           {queryData.map((product) => (
             <Grid key={product.id} item lg={3} md={4} sm={6} xs={12}>
-              <QueryItem product={product} />
+              <QueryItem product={product} onAddToCart={handleAddToCart} />
             </Grid>
           ))}
         </Grid>
