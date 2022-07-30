8828bb50ba36233d405e9c8c0ffdadb289d3549b
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-30 12:16:44 +0100

    Update CategoryButton.jsx

--- src/components/CategoryButton/CategoryButton.jsx
+++ src/components/CategoryButton/CategoryButton.jsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { Button } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  Stack,
+  Typography,
+} from "@mui/material";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
@@ -7,19 +14,33 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 import FilterListRoundedIcon from "@mui/icons-material/FilterListRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import { ColorButton } from "./styles";
+import { useCommerce } from "../../contexts/CommerceContext";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
 const CategoryButton = () => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const { sortByName } = useCommerce();
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
+    setOpen(false);
+  };
+
+  const sortingByAsc = () => {
+    sortByName("asc");
+    setOpen(false);
+  };
+
+  const sortingByDesc = () => {
+    sortByName("desc");
     setOpen(false);
   };
   return (
@@ -38,12 +59,31 @@ const CategoryButton = () => {
         onClose={handleClose}
         aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+        <Box
+          sx={{
+            padding: 2,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <IconButton onClick={handleClose}>
+            <CloseRoundedIcon />
+          </IconButton>
+          <Typography variant="h6" align="left">
+            Filters
+          </Typography>
+        </Box>
+        <Divider />
+        <DialogTitle>{"Sort Direction"}</DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-slide-description">
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
+            The direction to sort in, either ascending or descending
           </DialogContentText>
+          <Stack direction="row" spacing={1}>
+            <ColorButton onClick={sortingByAsc}>Ascending Order</ColorButton>
+            <ColorButton onClick={sortingByDesc}>Descending Order</ColorButton>
+          </Stack>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Disagree</Button>
