8fa294c6a148b81deb15ffa642d42a61406dae03
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-08-01 11:34:42 +0100

    Update CheckOut.jsx

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -1,32 +1,78 @@
 import React, { useEffect, useState } from "react";
+import { Box, Container, Paper, Step } from "@mui/material";
+import { StepLabel, Stepper, Typography } from "@mui/material";
+
+import commerce from "../../lib/commerce";
 import { useCommerce } from "../../contexts/CommerceContext";
-import { commerce } from "../../lib/commerce";
+
+import Confirmation from "./Confirmation";
+import AddressForm from "./AddressForm";
+import PaymentForm from "./PaymentForm";
+
+const steps = ["Shipping", "Payment"];
 
 const CheckOut = () => {
+  const [shippingData, setShippingData] = useState({});
+  const [checkoutToken, setCheckoutToken] = useState({});
+  const [activeStep, setActiveStep] = useState(0);
   const { cart } = useCommerce();
 
-  const [checkOutToken, setCheckOutToken] = useState({});
-
-  console.log(checkOutToken);
-
   useEffect(() => {
-    const getCheckOutToken = async () => {
-      if (cart.line_items.length) {
-        try {
-          const token = await commerce.checkout.generateToken(cart.id, {
-            type: "cart",
-          });
-          setCheckOutToken({ token });
-        } catch (error) {
-          console.log("There was an error generating token", error);
-        }
-      }
-    };
+    if (cart.line_items) {
+      generateCheckoutToken();
+    }
+  }, []);
 
-    getCheckOutToken();
-  }, [cart]);
+  const generateCheckoutToken = () => {
+    commerce.checkout
+      .generateToken(cart.id, { type: "cart" })
+      .then((token) => {
+        setCheckoutToken(token);
+      })
+      .catch((error) => {
+        console.log("There was an error in generating a token", error);
+      });
+  };
 
-  return <div>CheckOut</div>;
+  const nextStep = () => setActiveStep((prevStep) => prevStep + 1);
+  const backStep = () => setActiveStep((prevStep) => prevStep - 1);
+
+  const next = (data) => {
+    setShippingData(data);
+
+    nextStep();
+  };
+
+  const Test = () =>
+    activeStep === 0 ? (
+      <AddressForm {...{ checkoutToken, next }} />
+    ) : (
+      <PaymentForm {...{ shippingData, checkoutToken, backStep, nextStep }} />
+    );
+
+  return (
+    <Container maxWidth="md">
+      <Paper variant="outlined" sx={{ p: 4, mb: 5 }}>
+        <Box>
+          <Typography align="center" variant="h5" fontWeight={700} gutterBottom>
+            Checkout
+          </Typography>
+        </Box>
+        <Stepper activeStep={activeStep}>
+          {steps.map((step) => (
+            <Step key={step}>
+              <StepLabel>{step}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        {activeStep === steps.length ? (
+          <Confirmation />
+        ) : (
+          checkoutToken.id && <Test />
+        )}
+      </Paper>
+    </Container>
+  );
 };
 
 export default CheckOut;
