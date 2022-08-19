81ebe31dc59ce685762175917a893154a862d09b
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-08-19 11:14:05 +0100

    Update PaymentForm.jsx

--- src/pages/CheckOut/PaymentForm.jsx
+++ src/pages/CheckOut/PaymentForm.jsx
@@ -1,104 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
-import {
-  Elements,
-  CardElement,
-  ElementsConsumer,
-} from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
 import PropTypes from "prop-types";
 
 import PurchaseSummary from "./PurchaseSummary";
 import { useCommerce } from "../../contexts/CommerceContext";
 import { SpaceBtwFlexItems } from "../../themes/universalStyles";
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+const initialValues = {
+  cardNumber: "",
+  expiryMonth: "",
+  expiryYear: "",
+  ccv: "",
+  billingPostalZipcode: "",
+  showPassword: false,
+};
 
 const PaymentForm = ({ shippingData, checkoutToken, backStep, nextStep }) => {
   const { handleCaptureCheckout } = useCommerce();
-  console.log(shippingData);
+  const [values, setValues] = useState(initialValues);
 
-  const handleSubmit = async (event, elements, stripe) => {
-    event.prventDefault();
+  const handleChange = (prop) => (e) => {
+    setValues({ ...values, [prop]: e.target.value });
+  };
 
-    if (!stripe || !elements) return;
-
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: "card",
-      card: elements.getElement(CardElement),
-    });
-
-    if (error) {
-      console.log(error);
-    } else {
-      const orderData = {
-        line_items: checkoutToken.line_items,
-        customer: {
-          firstname: shippingData.firstname,
-          lastname: shippingData.lastname,
-          email: shippingData.email,
+  const handleSubmit = () => {
+    const orderData = {
+      line_items: checkoutToken.line_items,
+      customer: {
+        firstname: shippingData.firstname,
+        lastname: shippingData.lastname,
+        email: shippingData.email,
+      },
+      shipping: {
+        name: "Primary",
+        street: shippingData.address,
+        town_city: shippingData.city,
+        county_state: shippingData.shippingSubdivision,
+        postal_zip_code: shippingData.postalcode,
+        country: shippingData.shippingCountry,
+      },
+      fulfillment: {
+        shipping_method: shippingData.shippingOption,
+      },
+      payment: {
+        gateway: "test_gateway",
+        card: {
+          number: values.cardNumber,
+          expiry_month: values.expiryMonth,
+          expiry_year: values.expiryYear,
+          cvc: values.ccv,
+          postal_zip_code: values.billingPostalZipcode,
         },
-        shipping: {
-          name: "Primary",
-          street: shippingData.address,
-          town_city: shippingData.city,
-          county_state: shippingData.shippingSubdivision,
-          postal_zip_code: shippingData.postalcode,
-          country: shippingData.shippingCountry,
-        },
-        fulfillment: {
-          shipping_method: shippingData.shippingOption,
-        },
-        payment: {
-          gateway: "gway_9l6LJmxJR4mbo1",
-          stripe: {
-            payment_method_id: paymentMethod.id,
-          },
-        },
-      };
-      handleCaptureCheckout(checkoutToken.id, orderData);
-      nextStep();
-    }
+      },
+    };
+    handleCaptureCheckout(checkoutToken.id, orderData);
+    nextStep();
   };
 
   return (
     <Box>
-      <Box>
-        <PurchaseSummary {...{ checkoutToken }} />
-        <Typography sx={{ mt: 2 }} variant="h6" align="center" gutterBottom>
-          Payment Methods
-        </Typography>
-        <Elements stripe={stripePromise}>
-          <ElementsConsumer>
-            {({ stripe, elements }) => (
-              <Box
-                component="form"
-                onSubmit={(e) => handleSubmit(e, elements, stripe)}
-              >
-                <CardElement />
-                <SpaceBtwFlexItems sx={{ mt: 4 }}>
-                  <Button
-                    variant="contained"
-                    onClick={backStep}
-                    disableElevation
-                  >
-                    Back
-                  </Button>
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    color="success"
-                    disabled={!stripe}
-                    disableElevation
-                  >
-                    Pay {checkoutToken.live.subtotal.formatted_with_symbol}
-                  </Button>
-                </SpaceBtwFlexItems>
-              </Box>
-            )}
-          </ElementsConsumer>
-        </Elements>
-      </Box>
+      <PurchaseSummary {...{ checkoutToken }} />
+      <Typography sx={{ mt: 2 }} variant="h6" align="center" gutterBottom>
+        Payment Methods
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={values.cardNumber}
+          onChange={handleChange("cardNumber")}
+        />
+        <input
+          value={values.expiryMonth}
+          onChange={handleChange("expiryMonth")}
+        />
+        <input
+          value={values.expiryYear}
+          onChange={handleChange("expiryYear")}
+        />
+        <input value={values.ccv} onChange={handleChange("ccv")} />
+        <input
+          value={values.billingPostalZipcode}
+          onChange={handleChange("billingPostalZipcode")}
+        />
+        <SpaceBtwFlexItems sx={{ mt: 4 }}>
+          <Button variant="contained" onClick={backStep} disableElevation>
+            Back
+          </Button>
+          <Button
+            type="submit"
+            variant="contained"
+            color="success"
+            disableElevation
+          >
+            Pay {checkoutToken.live.subtotal.formatted_with_symbol}
+          </Button>
+        </SpaceBtwFlexItems>
+      </form>
     </Box>
   );
 };
