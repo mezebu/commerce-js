03f003acf712adc7207d6ac6d086f2dab081f781
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-08-20 13:52:44 +0100

    Update PurchaseSummary.jsx

--- src/pages/CheckOut/PurchaseSummary.jsx
+++ src/pages/CheckOut/PurchaseSummary.jsx
@@ -1,53 +1,61 @@
-import React from "react";
-import { Box, Typography, List, ListItem, Avatar } from "@mui/material";
-import { Divider, ListItemText, ListItemAvatar } from "@mui/material";
+import React, { Fragment } from "react";
+import { Box, Typography, Avatar, List, ListItemAvatar } from "@mui/material";
+import { Divider, ListItem, ListItemText } from "@mui/material";
+import PropTypes from "prop-types";
 
 const PurchaseSummary = ({ checkoutToken }) => {
-  console.log(checkoutToken);
   return (
     <Box>
       <Typography variant="h6" align="center" sx={{ mt: 2 }} gutterBottom>
         Order Summary
       </Typography>
-      {checkoutToken.line_items.map(
-        ({ id, image, name, quantity, line_total }) => (
-          <List key={id} sx={{ width: "100%", bgcolor: "background.paper" }}>
-            <ListItem
-              secondaryAction={
-                <Typography>{line_total?.formatted_with_symbol}</Typography>
-              }
-            >
-              <ListItemAvatar>
-                <Avatar alt={name} src={image?.url} />
-              </ListItemAvatar>
-              <ListItemText
-                primary={
-                  <Typography variant="subtitle1" sx={{ fontWeight: 600 }}>
-                    {name}
-                  </Typography>
-                }
-                secondary={
-                  <React.Fragment>
-                    <Typography
-                      sx={{ display: "inline" }}
-                      component="span"
-                      variant="subtitle2"
-                      color="text.primary"
-                    >
-                      Quantity: {quantity}
+      <Box sx={{ m: 5 }}>
+        {checkoutToken.line_items.map(
+          ({ id, image, name, quantity, line_total }) => (
+            <Fragment key={id}>
+              <List
+                sx={{
+                  width: "100%",
+                  bgcolor: "background.paper",
+                }}
+              >
+                <ListItem
+                  secondaryAction={
+                    <Typography variant="subtitle2" sx={{ fontWeight: 600 }}>
+                      {line_total?.formatted_with_symbol}
                     </Typography>
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-          </List>
-        )
-      )}
-      <Box sx={{ p: 3 }}>
-        <Typography variant="subtitle1" sx={{ fontWeight: 600 }}>
+                  }
+                >
+                  <ListItemAvatar>
+                    <Avatar alt={name} src={image?.url} />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={name}
+                    secondary={
+                      <Fragment>
+                        <Typography
+                          sx={{ display: "inline" }}
+                          component="span"
+                          variant="body2"
+                          color="text.primary"
+                        >
+                          Qunatity: {quantity}
+                        </Typography>
+                      </Fragment>
+                    }
+                  />
+                </ListItem>
+              </List>
+              <Divider />
+            </Fragment>
+          )
+        )}
+      </Box>
+      <Box>
+        <Typography variant="subtitle1" sx={{ fontWeight: 600 }} gutterBottom>
           Total
         </Typography>
-        <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
+        <Typography variant="subtitle1" sx={{ fontWeight: 700 }} gutterBottom>
           {checkoutToken.live.subtotal.formatted_with_symbol}
         </Typography>
       </Box>
@@ -57,3 +65,7 @@ const PurchaseSummary = ({ checkoutToken }) => {
 };
 
 export default PurchaseSummary;
+
+PurchaseSummary.propType = {
+  checkoutToken: PropTypes.object,
+};
