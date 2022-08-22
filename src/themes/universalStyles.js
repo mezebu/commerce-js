899e7d411736c9ae29bf38764a0a4ebc7fc173aa
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-08-22 10:03:02 +0100

    Update universalStyles.js

--- src/themes/universalStyles.js
+++ src/themes/universalStyles.js
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { createTheme, styled } from "@mui/material/styles";
 
 export const SpaceBtwFlexItems = styled(Box)({
   display: "flex",
@@ -23,4 +23,45 @@ export const CenteredFlexDirection = styled(Box)({
 export const JustifyCenter = styled(Box)({
   display: "flex",
   justifyContent: "center",
+});
+
+export const AlignCenter = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+});
+
+export const darkMode = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily: "Montserrat",
+      textTransform: "none",
+    },
+  },
+  shape: {
+    borderRadius: 5,
+  },
+  palette: {
+    mode: "dark",
+  },
+});
+
+export const lightMode = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily: "Montserrat",
+      textTransform: "none",
+    },
+  },
+  shape: {
+    borderRadius: 5,
+  },
+  palette: {
+    mode: "light",
+    background: {
+      default: "#F6F9FC",
+    },
+    primary: {
+      main: "#1fa2ff",
+    },
+  },
 });
