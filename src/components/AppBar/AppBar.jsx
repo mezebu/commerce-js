f03c43bf5ccf40dcd9201d85a83c84cc3492d06c
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-08-22 10:02:20 +0100

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,81 +1,123 @@
 import React from "react";
-import { Typography, Avatar, IconButton, Container } from "@mui/material";
 import { AppBar as MUIAppBar, Box, Badge, Toolbar } from "@mui/material";
+import { Typography, Avatar, IconButton } from "@mui/material";
+import { Container, Tooltip } from "@mui/material";
 import { useNavigate, useLocation } from "react-router-dom";
-import PropTypes from "prop-types";
+import { ThemeProvider } from "@mui/material/styles";
+import { Link } from "react-router-dom";
+import StoreMallDirectoryIcon from "@mui/icons-material/StoreMallDirectory";
 import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
+import LightModeRoundedIcon from "@mui/icons-material/LightModeRounded";
+import ModeNightRoundedIcon from "@mui/icons-material/ModeNightRounded";
 import SearchIcon from "@mui/icons-material/Search";
 import MenuIcon from "@mui/icons-material/Menu";
+import PropTypes from "prop-types";
 
 import { Search, SearchIconWrapper, StyledInputBase } from "./styles";
-import { Link } from "react-router-dom";
+// prettier-ignore
+import { useThemeContext, useThemeUpdateContext,} from "../../contexts/ThemeContext";
+import { lightMode, darkMode } from "./styles";
 import { useCommerce } from "../../contexts/CommerceContext";
 
 const AppBar = ({ handleDrawerToggle }) => {
   const { searchProduct, query, handleChange, cart } = useCommerce();
-  const navigate = useNavigate();
   const { pathname } = useLocation();
+  const darkTheme = useThemeContext();
+  const theme = darkTheme ? darkMode : lightMode;
+  /* const match = useTheme();
+  const isMobile = useMediaQuery(match.breakpoints.down("md")); */
+  const themeHandler = useThemeUpdateContext();
+  const navigate = useNavigate();
+  const icon = darkTheme ? <LightModeRoundedIcon /> : <ModeNightRoundedIcon />;
+  const title = darkTheme ? "Switch to light theme" : "Switch to dark theme";
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <MUIAppBar
-        component="nav"
-        elevation={0}
-        sx={{
-          background: "rgba( 255, 255, 255, 0.4 )",
-          backdropFilter: "blur( 4px )",
-          WebkitBackdropFilter: "blur( 4px )",
-          border: "1px solid rgba( 255, 255, 255, 0.18 )",
-        }}
-      >
-        <Container maxWidth="xl">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: "none" } }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-            >
-              <Link to="/">Logo</Link>
-            </Typography>
-            <Search onSubmit={searchProduct}>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="Search Product"
-                inputProps={{ "aria-label": "search" }}
-                value={query}
-                onChange={handleChange}
-              />
-            </Search>
-            <Box sx={{ p: 2 }}>
-              {pathname !== "/cart" && (
-                <Badge badgeContent={cart.total_items} color="primary">
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <MUIAppBar
+          color="inherit"
+          component="nav"
+          elevation={0}
+          sx={{
+            background: "rgba( 255, 255, 255, 0 )",
+            backdropFilter: "blur( 20px )",
+            WebkitBackdropFilter: "blur( 20px )",
+            border: "1px solid rgba( 255, 255, 255, 0.18 )",
+          }}
+        >
+          <Container maxWidth="xl">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { sm: "none" } }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Box sx={{ flexGrow: 1 }}>
+                {pathname !== "/search" && (
+                  <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{ ml: 1, display: { xs: "none", sm: "block" } }}
+                  >
+                    <Link to="/">Logo</Link>
+                  </Typography>
+                )}
+              </Box>
+
+              <Search onSubmit={searchProduct}>
+                <SearchIconWrapper>
+                  <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Search Product"
+                  inputProps={{ "aria-label": "search" }}
+                  value={query}
+                  onChange={handleChange}
+                />
+              </Search>
+              <Box sx={{ p: 2 }}>
+                {pathname !== "/cart" && (
+                  <Badge badgeContent={cart.total_items} color="primary">
+                    <Avatar>
+                      <IconButton
+                        aria-label="shopping-cart"
+                        onClick={() => navigate("/cart")}
+                      >
+                        <LocalMallOutlinedIcon />
+                      </IconButton>
+                    </Avatar>
+                  </Badge>
+                )}
+              </Box>
+              <Box sx={{ mr: 2 }}>
+                {pathname === "/search" && (
                   <Avatar>
-                    <IconButton
-                      aria-label="shopping-cart"
-                      onClick={() => navigate("/cart")}
-                    >
-                      <LocalMallOutlinedIcon />
-                    </IconButton>
+                    <Tooltip title="Back to products page">
+                      <IconButton
+                        aria-label="shopping-cart"
+                        onClick={() => navigate("/products")}
+                      >
+                        <StoreMallDirectoryIcon />
+                      </IconButton>
+                    </Tooltip>
                   </Avatar>
-                </Badge>
-              )}
-            </Box>
-          </Toolbar>
-        </Container>
-      </MUIAppBar>
-    </Box>
+                )}
+              </Box>
+              <Tooltip title={title}>
+                <Avatar>
+                  <IconButton onClick={themeHandler}>{icon}</IconButton>
+                </Avatar>
+              </Tooltip>
+            </Toolbar>
+          </Container>
+        </MUIAppBar>
+      </Box>
+    </ThemeProvider>
   );
 };
 
