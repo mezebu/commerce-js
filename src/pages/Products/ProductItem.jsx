98c7008808c4b8842fec3fa2ab05081e5aa18db4
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-10-04 09:55:17 +0100

    Update ProductItem.jsx

--- src/pages/Products/ProductItem.jsx
+++ src/pages/Products/ProductItem.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import { CardMedia, Typography, IconButton } from "@mui/material";
-import { CardActions, CardContent } from "@mui/material";
+import { Card, CardContent, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import AddShoppingCartRoundedIcon from "@mui/icons-material/AddShoppingCartRounded";
 import RemoveRedEyeRoundedIcon from "@mui/icons-material/RemoveRedEyeRounded";
+import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
 import PropTypes from "prop-types";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
-
-import { ProductWrapper } from "./styles";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -38,30 +36,42 @@ const ProductItem = ({ product, onAddToCart }) => {
   const { image, name, price, id } = product;
 
   return (
-    <ProductWrapper elevation={0} variant="outlined">
-      <CardMedia component="img" height="190" image={image?.url} />
-      <CardContent>
-        <Typography sx={{ fontWeight: 600 }} gutterBottom variant="body1">
-          {name}
-        </Typography>
-        <Typography sx={{ fontWeight: 500 }} gutterBottom variant="body2">
-          {price?.formatted_with_symbol}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton color="primary" onClick={handleAddToCart}>
-          <AddShoppingCartRoundedIcon />
-        </IconButton>
-        <IconButton color="primary" onClick={() => handleDescription(id)}>
-          <RemoveRedEyeRoundedIcon />
-        </IconButton>
-      </CardActions>
-      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
-          {name} has been added to your cart
-        </Alert>
-      </Snackbar>
-    </ProductWrapper>
+    <>
+      <Card sx={{ display: "flex" }} elevation={0} variant="outlined">
+        <CardMedia
+          component="img"
+          sx={{ width: 160 }}
+          height="130"
+          image={image?.url}
+          alt={name}
+        />
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <CardContent sx={{ flex: "1 0 auto" }}>
+            <Typography variant="subtitle2">{name}</Typography>
+            <Typography variant="subtitle2" color="text.secondary">
+              {price?.formatted_with_symbol}
+            </Typography>
+          </CardContent>
+          <Box sx={{ display: "flex", alignItems: "center", pl: 1 }}>
+            <IconButton color="primary" onClick={handleAddToCart}>
+              <AddCircleRoundedIcon />
+            </IconButton>
+            <IconButton color="primary" onClick={() => handleDescription(id)}>
+              <RemoveRedEyeRoundedIcon />
+            </IconButton>
+          </Box>
+        </Box>
+        <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            sx={{ width: "100%" }}
+          >
+            {name} has been added to your cart
+          </Alert>
+        </Snackbar>
+      </Card>
+    </>
   );
 };
 
