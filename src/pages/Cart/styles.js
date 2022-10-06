518b3796dcf8365db076ebe0955f556a72480dd9
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-10-06 10:33:03 +0100

    Update styles.js

--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,15 +1,5 @@
-import { Box, TableCell, tableCellClasses } from "@mui/material";
+import { Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
-
-export const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: "#475569",
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
 
 export const ActionButtons = styled(Box)(({ theme }) => ({
   border: "1px solid rgba(145, 158, 171, 0.32)",
@@ -17,6 +7,14 @@ export const ActionButtons = styled(Box)(({ theme }) => ({
   display: "flex",
   alignItems: "center",
   justifyContent: "space-between",
-  width: 90,
-  height: 35,
+  width: 70,
+  height: 25,
+}));
+
+export const StyledCartItems = styled(Box)(({ theme }) => ({
+  borderTop: `1px solid ${theme.palette.mode === "light" ? "#000" : "#fff"}`,
+  borderBottom: `1px solid ${theme.palette.mode === "light" ? "#000" : "#fff"}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
 }));
