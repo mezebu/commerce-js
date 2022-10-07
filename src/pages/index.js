44c7b779ef2f6153d2f43ed81b2c6c6cfcc95077
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-10-07 08:35:35 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 export { default as Hero } from "./HeroPage/Hero";
 export { default as ProductsList } from "./Products/ProductsList";
-export { default as ProductDescription } from "./Products/ProductDescription";
 export { default as Cart } from "./Cart/Cart";
 export { default as CheckOut } from "./CheckOut/CheckOut";
 export { default as NoMatch } from "./NoMatch/NoMatch";
