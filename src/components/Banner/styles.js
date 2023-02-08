f3fb1e6e6554d4649ac24787fb3cab117466fcb7
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-02-08 10:47:06 +0100

    Create styles.js

---
+++ src/components/Banner/styles.js
@@ -0,0 +1,10 @@
+import { styled } from "@mui/material/styles";
+import { Box } from "@mui/material";
+
+export const StyledBanner = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+
+  alignItems: "center",
+  padding: 30,
+});
