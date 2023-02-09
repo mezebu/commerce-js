d202c2c29018958aeb460bac418059bc7fd8d509
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-02-09 09:07:09 +0100

    Revert "Update SearchBar.jsx"
    This reverts commit af464d71ca4bb6f0aef21290341cf8ac2f93708f.

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,26 +1,30 @@
 import React from "react";
-import SearchIcon from "@mui/icons-material/Search";
 import { Box, TextField } from "@mui/material";
 
 import { useCommerce } from "../../contexts/CommerceContext";
-import { Search, SearchIconWrapper, StyledInputBase } from "./styles";
+import FilterButton from "../FilterButton/FilterButton";
 
 const SearchBar = () => {
   const { searchProduct, query, handleChange } = useCommerce();
 
   return (
-    <Search onSubmit={searchProduct}>
-      <SearchIconWrapper>
-        <SearchIcon />
-      </SearchIconWrapper>
-      <StyledInputBase
-        label="Search Product"
-        value={query}
-        onChange={handleChange}
-        inputProps={{ "aria-label": "search" }}
-        placeholder="Search Product…"
-      />
-    </Search>
+    <Box
+      sx={{ display: "flex", alignItems: "center ", justifyContent: "center" }}
+    >
+      <Box component="form" onSubmit={searchProduct} sx={{ width: "40rem" }}>
+        <TextField
+          inputProps={{ "aria-label": "search" }}
+          variant="filled"
+          label="Search Product"
+          value={query}
+          onChange={handleChange}
+          fullWidth
+        />
+      </Box>
+      <Box sx={{ ml: 2 }}>
+        <FilterButton />
+      </Box>
+    </Box>
   );
 };
 
