e00cacbcf841cbf358b79046f26c6fb46be0df0b
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-02-09 11:30:14 +0100

    Update styles.js

--- src/components/Categories/styles.js
+++ src/components/Categories/styles.js
@@ -1,11 +1,48 @@
-import { styled } from "@mui/material/styles";
-import { Button } from "@mui/material";
+import { alpha, styled } from "@mui/material/styles";
+import { Menu } from "@mui/material";
 
-export const StyledButton = styled(Button)(({ theme }) => ({
-  "&:focus": {
+export const StyledMenu = styled((props) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "right",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "right",
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiPaper-root": {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 180,
     backgroundColor:
+      theme.palette.mode === "light" ? "white" : theme.palette.background.paper,
+    fontFamily: "Montserrat",
+    color:
       theme.palette.mode === "light"
-        ? theme.palette.grey[100]
-        : theme.palette.grey[800],
+        ? "rgb(55, 65, 81)"
+        : theme.palette.grey[300],
+    boxShadow:
+      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+    "& .MuiMenu-list": {
+      padding: "4px 0",
+    },
+    "& .MuiMenuItem-root": {
+      "& .MuiSvgIcon-root": {
+        fontSize: 18,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      "&:active": {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity
+        ),
+      },
+    },
   },
 }));
