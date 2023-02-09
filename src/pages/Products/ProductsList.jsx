0b889c6ef7c93aabff55bc87074105171f9dd31a
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-02-09 11:32:43 +0100

    Update ProductsList.jsx

--- src/pages/Products/ProductsList.jsx
+++ src/pages/Products/ProductsList.jsx
@@ -1,53 +1,44 @@
 import React from "react";
-import { Box, CircularProgress, Container, Grid } from "@mui/material";
+import { Box, CircularProgress, Grid } from "@mui/material";
 
 import { useCommerce } from "../../contexts/CommerceContext";
 
 import ProductItem from "./ProductItem";
 import SearchBar from "../../components/SearchBar/SearchBar";
-import Categories from "../../components/Categories/Categories";
 
 const ProductsList = () => {
   const { products, handleAddToCart, loading } = useCommerce();
 
   return (
-    <Container>
-      <Box sx={{ mb: 5 }}>
-        <Box>
-          <Categories />
-        </Box>
-        <Box sx={{ py: 3 }}>
-          <SearchBar />
-        </Box>
-        <Grid container spacing={3}>
-          {loading ? (
-            <Grid item lg={12} md={12} sm={12} xs={12}>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  height: "50vh",
-                }}
-              >
-                <CircularProgress />
-              </Box>
-            </Grid>
-          ) : (
-            <>
-              {products.map((product) => (
-                <Grid key={product.id} item lg={4} md={4} sm={6} xs={12}>
-                  <ProductItem
-                    product={product}
-                    onAddToCart={handleAddToCart}
-                  />
-                </Grid>
-              ))}
-            </>
-          )}
-        </Grid>
+    <Box sx={{ mb: 5 }}>
+      <Box sx={{ py: 3 }}>
+        <SearchBar />
       </Box>
-    </Container>
+      <Grid container spacing={1}>
+        {loading ? (
+          <Grid item lg={12} md={12} sm={12} xs={12}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "50vh",
+              }}
+            >
+              <CircularProgress />
+            </Box>
+          </Grid>
+        ) : (
+          <>
+            {products.map((product) => (
+              <Grid key={product.id} item lg={3} md={4} sm={6} xs={12}>
+                <ProductItem product={product} onAddToCart={handleAddToCart} />
+              </Grid>
+            ))}
+          </>
+        )}
+      </Grid>
+    </Box>
   );
 };
 
